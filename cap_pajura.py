68f7f43d5959fb2cf930c09bdf568373cd618ac5
Sebastian PC <bjsarmy@gmail.com>
2023-06-13 08:20:32 +0300

    Added an if statement to cap_pajura

--- cap_pajura.py
+++ cap_pajura.py
@@ -3,21 +3,24 @@ import random
 
 print("Bun venit din partea creatorului meu Bejenaru Sebastian!\nPoftim o moneda de 50 de bani, dai cu banul!")
 choice = input("Cap sau pajura?\n").lower()
-time.sleep(1)
-print("Ai dat cu banul!")
-time.sleep(1)
-random_int = random.randint(0, 1)
-if random_int == 1:
-    print("A rezultat Cap!")
-    time.sleep(1)
-    if choice == "cap":
+if choice == "cap" or choice == "pajura":
+  time.sleep(1)
+  print("Ai dat cu banul!")
+  time.sleep(1)
+  random_int = random.randint(0, 1)
+  if random_int == 1:
+      print("A rezultat Cap!")
+      time.sleep(1)
+      if choice == "cap":
         print(f"Pentru ca ai ales {choice}, ai castigat!")
-    else:
+      else:
+        print(f"Pentru ca ai ales {choice}, ai pierdut!")
+  elif random_int == 0:
+      print("A rezultat Pajura!")
+      time.sleep(1)
+      if choice == "pajura":
+        print(f"Pentru ca ai ales {choice}, ai castigat!")
+      else:
         print(f"Pentru ca ai ales {choice}, ai pierdut!")
 else:
-    print("A rezultat Pajura!")
-    time.sleep(1)
-    if choice == "pajura":
-        print(f"Pentru ca ai ales {choice}, ai castigat!")
-    else:
-        print(f"Pentru ca ai ales {choice}, ai pierdut!")
+  print("Nu ai selectat o optiune corecta, incearca din nou.")
